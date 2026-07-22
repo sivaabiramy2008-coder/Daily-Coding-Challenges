f42c8b8ee91e398dd0a104c511e4f6cf0b438202
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-07-22 22:19:20 +0530

    Implement missing number finder in Missing Number.java

---
+++ DAY 10/Missing Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int expectsum=n*(n+1)/2;
+        int actualsum=0;
+        for(int i=0;i<nums.length;i++){
+            actualsum+=nums[i];
+        }
+        return expectsum-actualsum;
+    }
+}
