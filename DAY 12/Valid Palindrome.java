793fc81f76381d38024d68299f3cf35a20c08ba6
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-07-24 19:53:04 +0530

    Implement palindrome check in Valid Palindrome class

---
+++ DAY 12/Valid Palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                str += Character.toLowerCase(c);
+            }
+        }
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
