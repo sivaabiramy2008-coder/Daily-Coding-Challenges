53ce6ff7174c4eed8fe6b930cc11186f52b5c2ec
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-07-27 21:31:41 +0530

    Implement house robber solution in Java

---
+++ DAY 14/House Rubber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int take=nums[i]+dp[i-2];
+            int skip=dp[i-1];
+            dp[i]=Math.max(take,skip);
+        
+        }
+        return dp[nums.length-1];
+    }
+}
