a84a6aee866e13e9182eeaf9872f3a312a601961
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-07-27 21:14:52 +0530

    Implement leftRightDifference method in Solution class

---
+++ DAY 13/Left and Right Sum Differences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int totalSum=0;
+        for(int num:nums){
+            totalSum+=num;
+        }
+        int leftSum=0;
+        int rightSum=totalSum;
+        int[] answer=new int[nums.length]; 
+        for (int i=0;i<nums.length;i++){
+            rightSum-=nums[i];
+            answer[i]=Math.abs(leftSum-rightSum);
+            leftSum+=nums[i];
+       }
+        return answer;
+    }
+}
