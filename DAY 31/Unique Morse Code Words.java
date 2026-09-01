4bdba71ab35e2c174124286a2e1059ebc9ee9147
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-09-01 20:55:46 +0530

    Create Unique Morse Code Words.java

---
+++ DAY 31/Unique Morse Code Words.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+            "....", "..", ".---", "-.-", ".-..", "--", "-.",
+            "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+            "...-", ".--", "-..-", "-.--", "--.."
+        };
+        HashSet<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                sb.append(morse[ch - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
