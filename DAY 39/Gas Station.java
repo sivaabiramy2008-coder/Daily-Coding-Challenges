710fb4de4cd077553583e4ca6e14ce34b03fc175
ABIRAMY S <sivaabiramy2008@gmail.com>
2026-09-16 22:58:09 +0530

    Create Gas Station.java

---
+++ DAY 39/Gas Station.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        int currentGas = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            currentGas += gas[i] - cost[i];
+            if (currentGas < 0) {
+                start = i + 1;
+                currentGas = 0;
+            }
+        }
+        if (totalGas < totalCost) {
+            return -1;
+        }
+        return start;
+    }
+}
